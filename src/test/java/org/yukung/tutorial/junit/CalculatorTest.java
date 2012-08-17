9ab6020969786bc6813c153d7bdfcf197a56e252
yukung <yukung.i@gmail.com>
2012-08-17 10:22:29 +0900

    乗算メソッドのテストを追加。

--- src/test/java/org/yukung/tutorial/junit/CalculatorTest.java
+++ src/test/java/org/yukung/tutorial/junit/CalculatorTest.java
@@ -1,5 +1,6 @@
 package org.yukung.tutorial.junit;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.After;
@@ -17,8 +18,11 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void multiplyで乗算結果が取得できる() {
+		Calculator calc = new Calculator();
+		int expected = 12;
+		int actual = calc.multiply(3, 4);
+		assertThat(actual, is(expected));
 	}
 
 }
