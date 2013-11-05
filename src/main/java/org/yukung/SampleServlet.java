14796b98e48bb663ecfc927ed7fa2ebea8de7369
Yusuke Ikeda <ikeda_yusuke@cyberagent.co.jp>
2013-11-05 18:37:27 +0900

    EJBのローカルインタフェースとリモートインタフェースを追加して呼び出すように変更

--- src/main/java/org/yukung/SampleServlet.java
+++ src/main/java/org/yukung/SampleServlet.java
@@ -9,17 +9,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.yukung.service.HelloEjb;
+import org.yukung.service.HelloEjbLocal;
+import org.yukung.service.HelloEjbRemote;
 
 @WebServlet(urlPatterns={"/servlet/SampleServlet"})
 public class SampleServlet extends HttpServlet {
 
 	@EJB
-	private HelloEjb hello;
+	private HelloEjbRemote helloRemote;
+
+	@EJB
+	private HelloEjbLocal helloLocal;
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		PrintWriter pw = response.getWriter();
 		pw.println("This is Servlet3.0");
-		pw.println(hello.greet("EJB"));
+		pw.println(helloLocal.greetLocal("EJB"));
+		pw.println(helloRemote.greetRemote("EJB"));
 	}
 }
--- src/main/java/org/yukung/service/HelloEjb.java
+++ src/main/java/org/yukung/service/HelloEjb.java
@@ -3,8 +3,11 @@ package org.yukung.service;
 import javax.ejb.Stateless;
 
 @Stateless
-public class HelloEjb {
-	public String greet(String str) {
+public class HelloEjb implements HelloEjbLocal, HelloEjbRemote {
+	public String greetLocal(String str) {
+		return "Hello, " + str + "!!";
+	}
+	public String greetRemote(String str) {
 		return "Hello, " + str + "!!";
 	}
 }
