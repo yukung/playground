73650a382d493e2473b12100e91d0aeac98650ed
Yusuke Ikeda <ikeda_yusuke@cyberagent.co.jp>
2013-11-07 15:06:56 +0900

    コントローラを Servlet から JSF へ変更。

---
+++ src/main/java/org/yukung/web/BookController.java
@@ -0,0 +1,36 @@
+package org.yukung.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+
+import org.yukung.domain.Book;
+import org.yukung.service.BookService;
+
+@ManagedBean
+public class BookController {
+	@EJB
+	private BookService bookService;
+	private Book book = new Book();
+	private List<Book> books = new ArrayList<Book>();
+
+	@PostConstruct
+	public void doPostConstruct() {
+		books = bookService.findAll();
+	}
+	public String listBooksAction() {
+		books = bookService.findAll();
+		return "listBooks.xhtml";
+	}
+	public String registerBookAction() {
+		book = bookService.create(book);
+		return "registeredBook.xhtml";
+	}
+	public Book getBook() { return book; }
+	public void setBook(Book book) { this.book = book; }
+	public List<Book> getBooks() { return books; }
+	public void setBooks(List<Book> books) { this.books = books; }
+}
