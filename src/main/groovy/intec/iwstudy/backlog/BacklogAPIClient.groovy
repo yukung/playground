ab019c9a6d9a32c22e03f5a583435a4448274711
yukung <yukung.i@gmail.com>
2014-05-10 22:52:53 +0900

    Add Backlog client and test case.

---
+++ src/main/groovy/intec/iwstudy/backlog/BacklogAPIClient.groovy
@@ -0,0 +1,4 @@
+package intec.iwstudy.backlog
+
+class BacklogAPIClient {
+}
---
+++ src/test/groovy/intec/iwstudy/backlog/BacklogAPIClientSpec.groovy
@@ -0,0 +1,6 @@
+package intec.iwstudy.backlog
+
+import spock.lang.Specification
+
+class BacklogAPIClientSpec extends Specification {
+}
