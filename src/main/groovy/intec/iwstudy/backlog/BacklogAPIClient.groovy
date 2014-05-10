0c3aaf6bb2d0a9391cff394e998c83ea3515fff3
yukung <yukung.i@gmail.com>
2014-05-10 23:02:11 +0900

    Add test case, and first implementation.

--- src/main/groovy/intec/iwstudy/backlog/BacklogAPIClient.groovy
+++ src/main/groovy/intec/iwstudy/backlog/BacklogAPIClient.groovy
@@ -1,4 +1,22 @@
 package intec.iwstudy.backlog
 
+import org.apache.xmlrpc.client.XmlRpcClient
+import org.apache.xmlrpc.client.XmlRpcClientConfigImpl
+
 class BacklogAPIClient {
+    private XmlRpcClient client
+
+    BacklogAPIClient(url, username, password) {
+        def config = new XmlRpcClientConfigImpl()
+        config.serverURL = new URL(url)
+        config.basicUserName = username
+        config.basicPassword = password
+        client = new XmlRpcClient()
+        client.setConfig(config)
+    }
+
+    @Override
+    Object invokeMethod(String name, Object args) {
+        return client.execute("backlog.$name", args as Object[])
+    }
 }
--- src/test/groovy/intec/iwstudy/backlog/BacklogAPIClientSpec.groovy
+++ src/test/groovy/intec/iwstudy/backlog/BacklogAPIClientSpec.groovy
@@ -1,6 +1,35 @@
 package intec.iwstudy.backlog
 
+import org.apache.xmlrpc.XmlRpcException
 import spock.lang.Specification
 
 class BacklogAPIClientSpec extends Specification {
+    static final BACKLOG_API_URL = 'https://intec.backlog.jp/XML-RPC'
+    static final PROJECT_KEY = 'IWSTUDY'
+    static final USER_NAME = 'yusukei'
+    static final PASSWORD = 'xxxxxxxxxxxx' // FIX ME the valid password here.
+
+    def client = new BacklogAPIClient(BACKLOG_API_URL, USER_NAME, PASSWORD)
+
+    def "BacklogAPIClient のインスタンスを取得できること"() {
+        expect:
+        client != null
+    }
+
+    def "BacklogAPIにアクセスするのに必要な Basic 認証が成功してAPIアクセスができること"() {
+        when:
+        def projects = client.getProjects()
+        then:
+        notThrown(XmlRpcException)
+    }
+
+    def "BacklogAPIにアクセスするのに必要な Basic 認証がパスワードが妥当でなく 401 Unauthorized で返ってくること"() {
+        when: "無効なパスワードが設定されている"
+        def client = new BacklogAPIClient(BACKLOG_API_URL, USER_NAME, "invalid password.")
+        client.getProjects()
+        then:
+        def e = thrown(XmlRpcException)
+        e.message.contains('Unauthorized')
+    }
+
 }
