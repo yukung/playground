4edc8b00862153ff48593a47e79669a23f78f77e
yukung <yukung.i@gmail.com>
2014-09-04 20:45:49 +0900

    Move the SQL definition to annotation from xml file.

--- src/main/java/org/yukung/sandbox/mybatis/user/mybatis/UserMapper.java
+++ src/main/java/org/yukung/sandbox/mybatis/user/mybatis/UserMapper.java
@@ -1,5 +1,7 @@
 package org.yukung.sandbox.mybatis.user.mybatis;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
 import org.yukung.sandbox.mybatis.user.User;
 
 import java.util.List;
@@ -9,9 +11,12 @@ import java.util.List;
  */
 public interface UserMapper {
 
+    @Select("SELECT * FROM user")
     List<User> findAll();
 
+    @Insert("INSERT INTO user (name, age, gender) VALUES (#{name}, #{age}, #{gender})")
     int insert(User user);
 
+    @Select("SELECT * FROM user WHERE id = #{id}")
     User findById(Long id);
 }
