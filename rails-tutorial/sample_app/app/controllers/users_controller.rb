a42cb0a17cac085924393f5ab217af260871d6ce
yukung <yukung.i@gmail.com>
2017-06-04 20:10:48 +0900

    Revert "7.4.1 演習"
    This reverts commit 90d3fadce1aadbc1467dc29b665a4b96c08cf4b9.

--- rails-tutorial/sample_app/app/controllers/users_controller.rb
+++ rails-tutorial/sample_app/app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_url(@user)
+      redirect_to @user
     else
       render 'new'
     end
