69b3c47a8e440fa7af000a0ce07c1dd3a255b33c
yukung <yukung.i@gmail.com>
2017-06-04 19:11:13 +0900

    7.3.1 正しいフォーム

--- rails-tutorial/sample_app/app/controllers/users_controller.rb
+++ rails-tutorial/sample_app/app/controllers/users_controller.rb
@@ -7,4 +7,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user]) # Rails 4.0 以降ではエラーとなる
+    if @user.save
+      # success
+    else
+      render 'new'
+    end
+  end
 end
