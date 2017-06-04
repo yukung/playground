1962e0cd2ab1757e6ae5e83d9b0184ee3d7aeba4
yukung <yukung.i@gmail.com>
2017-06-04 20:03:53 +0900

    7.4.1 登録フォームの完成

--- rails-tutorial/sample_app/app/controllers/users_controller.rb
+++ rails-tutorial/sample_app/app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # success
+      redirect_to @user
     else
       render 'new'
     end
