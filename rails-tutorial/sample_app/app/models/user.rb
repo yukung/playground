e2bb8468f96020788b9cee1974feb03c11cd0ef7
yukung <yukung.i@gmail.com>
2017-10-07 20:16:36 +0900

    9.1.4 2つの目立たないバグ

--- rails-tutorial/sample_app/app/models/user.rb
+++ rails-tutorial/sample_app/app/models/user.rb
@@ -28,6 +28,7 @@ class User < ApplicationRecord
 
   # 渡されたトークンがダイジェストと一致したら true を返す
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
