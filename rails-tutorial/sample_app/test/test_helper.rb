fec9b589b5935ce4f9de88e2a6f6bb1202538ba0
yukung <yukung.i@gmail.com>
2017-10-07 10:14:04 +0900

    8.2.5 ユーザー登録時にログイン

--- rails-tutorial/sample_app/test/test_helper.rb
+++ rails-tutorial/sample_app/test/test_helper.rb
@@ -10,4 +10,8 @@ class ActiveSupport::TestCase
   include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
+  # テストユーザーがログイン中の場合に true を返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
