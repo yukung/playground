ef61754f47d56afe3f37bacb4c32e78c0787581b
yukung <yukung.i@gmail.com>
2017-10-10 23:11:13 +0900

    10.1.3 編集失敗時のテスト

--- rails-tutorial/sample_app/test/integration/users_signup_test.rb
+++ rails-tutorial/sample_app/test/integration/users_signup_test.rb
@@ -5,7 +5,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post signup_path, params: {
+      post users_path, params: {
         user: {
           name: "",
           email: "user@invalid",
