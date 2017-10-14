bf7231e7f4e31d98d151ed1414345e6de5e7eb8c
yukung <yukung.i@gmail.com>
2017-10-14 16:11:42 +0900

    11.3.3 演習

--- rails-tutorial/sample_app/app/models/user.rb
+++ rails-tutorial/sample_app/app/models/user.rb
@@ -41,8 +41,8 @@ class User < ApplicationRecord
 
   # アカウントを有効にする
   def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at, Time.zone.now)
+    # 問い合わせ回数を減らす
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   # 有効化用のメールを送信する
