afe2d01de29176bec561be60651fb1d4f3acc8cb
yukung <yukung.i@gmail.com>
2017-12-02 15:39:11 +0900

    13.3.1 マイクロポストのアクセス制御

--- rails-tutorial/sample_app/app/controllers/users_controller.rb
+++ rails-tutorial/sample_app/app/controllers/users_controller.rb
@@ -52,15 +52,6 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    # before アクション
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
     # 正しいユーザかどうか確認
     def correct_user
       @user = User.find(params[:id])
