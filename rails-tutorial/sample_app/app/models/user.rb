1f0525b4dc5b7c202966ab31c4ba9532f4d89528
yukung <yukung.i@gmail.com>
2017-12-19 19:36:40 +0900

    14.1.4 フォローしているユーザ

--- rails-tutorial/sample_app/app/models/user.rb
+++ rails-tutorial/sample_app/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
@@ -72,6 +73,21 @@ class User < ApplicationRecord
   # 完全な実装は次章の「ユーザをフォローする」を参照
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  # ユーザをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # ユーザをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしていたら true を返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
