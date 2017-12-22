43480490bdeb21fe421d6006bba37fa4f85ebf08
yukung <yukung.i@gmail.com>
2017-12-22 20:04:20 +0900

    14.2.5 [Follow] ボタン（ Ajax 編）

--- rails-tutorial/sample_app/config/application.rb
+++ rails-tutorial/sample_app/config/application.rb
@@ -15,5 +15,8 @@ module SampleApp
     # To Avoid the "uninitialized constant #{model}::#{uploader_class_name} caused by Carrierwave.
     # See: https://github.com/carrierwaveuploader/carrierwave/issues/399#issuecomment-51422423
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+
+    # 認証トークンを remote フォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
